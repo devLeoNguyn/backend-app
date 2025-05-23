ab62223a0f2f56cb88b405eeb351cddcba5c2d8e
devLeoNguyen <leo0393790@gmail.com>
2025-05-23 13:11:13 +0700

    Fix route / to render index.hbs

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Welcome to the Movie Backend API!');
+  res.render('Welcome to the Movie Backend API!');
 });
 
 module.exports = router;
