daa052e73ebdc30b1f8145fc32c09f50036676e1
devLeoNguyn <151083002+devLeoNguyn@users.noreply.github.com>
2025-05-29 14:34:06 +0700

    update-swagger

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 // Swagger 
 const swaggerUi = require('swagger-ui-express');
@@ -13,6 +14,9 @@ var app = express();
 //connect moongose
 require('dotenv').config();
 const mongoose = require('mongoose');
+
+// Enable CORS
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -46,10 +50,9 @@ app.use('/api/payments', paymentRoutes);
 app.use('/api/ratings', ratingRoutes);
 app.use('/api/genres', genreRoutes);
 
-// Swagger
+// Swagger Documentation
 const swaggerPath = path.join(__dirname, 'swagger.yaml');
 const swaggerDocument = YAML.load(swaggerPath);
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // catch 404 and forward to error handler
